da3e5a6b0f77c8b47d3e2a47ac03525deca9ad9c
noah <noahlog@naver.com>
2019-10-01 15:24:38 +0900

    이벤트 생성 API 구현 : 201 응답 받기 TDD

--- src/test/java/com/example/demorest/events/EventControllerTests.java
+++ src/test/java/com/example/demorest/events/EventControllerTests.java
@@ -1,6 +1,7 @@
 package com.example.demorest.events;
 
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,7 +11,11 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.time.LocalDateTime;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
@@ -19,20 +24,40 @@ public class EventControllerTests {
 
 
     /*스프링 MVC 테스트 핵심 클래스
-    * 웹 서버를 띄우지 않고도 스프링 MVC (DispatcherServlet)가 요청을 처리하는 과정을 확인할 수 있기 때문에 컨트롤러 테스트용으로 자주 쓰임
-    * */
+     * 웹 서버를 띄우지 않고도 스프링 MVC (DispatcherServlet)가 요청을 처리하는 과정을 확인할 수 있기 때문에 컨트롤러 테스트용으로 자주 쓰임
+     * */
     @Autowired
     MockMvc mockMvc;
-    
+
+    @Autowired
+    ObjectMapper objectMapper;
+
 
     @Test
     public void createEvent() {
+
+        Event event = Event.builder()
+                .name("Spring")
+                .descrption("REST API Development wit Spring")
+                .beginEnrollmentDateTime(LocalDateTime.of(2019,10,1,12,11,10))
+                .closeEnrollmentDateTime(LocalDateTime.of(2019,10,5,11,20,13))
+                .beginEventDateTime(LocalDateTime.of(2019,10,10,10,10,10))
+                .endEventDateTime(LocalDateTime.of(2019,10,10,10,10,10))
+                .basePrice(100)
+                .maxPrice(200)
+                .limitOffEnrollment(100)
+                .location("광진구")
+                .build();
+
         try {
             mockMvc.perform(post("/api/events/")
-            .contentType(MediaType.APPLICATION_JSON)
-            .accept(MediaTypes.HAL_JSON))
-                    .andExpect(status().isCreated()
-                    );
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .accept(MediaTypes.HAL_JSON)
+                        .content(objectMapper.writeValueAsBytes(event)))
+                    .andDo(print())
+                    .andExpect(jsonPath("id").exists())
+                    .andExpect(status().isCreated());
+
 
         } catch (Exception e) {
             e.printStackTrace();
